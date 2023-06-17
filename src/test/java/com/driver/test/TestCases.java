b21659fa3f8cebd1c0428003c8e5a8323702b592
Bhoma-RamDaiya <bhoma05061998@gmail.com>
2023-06-17 11:51:12 +0530

    new Solution is adding

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,24 +1,32 @@
+//package com.driver.test;
+//
+//public class TestCases {
+//    public static class A{
+//        public String meth(){
+//            return "Invoking method from class A";
+//        }
+//    }
+//    public static class B extends A{
+//        @Override
+// public String meth(){
+//     return "method is overriden in extended class B";
+// }
+//    }
+//    public static void main(String[] args) {
+//        int x =0 ;
+// B b = new B();
+//        b.meth();
+// B b1 = new B();
+//
+//        b1.meth();
+//    }
+//
+//}
 package com.driver.test;
 
-public class TestCases {
-    public static class A{
-        public String meth(){
-            return "Invoking method from class A";
-        }
-    }
-    public static class B extends A{
-        @Override
- public String meth(){
-     return "method is overriden in extended class B";
- }
-    }
-    public static void main(String[] args) {
-        int x =0 ;
- B b = new B();
-        b.meth();
- B b1 = new B();
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-        b1.meth();
-    }
-    
+public class TestCases {
+
 }
